e789b25e628ac6a73dd5927ec6187ce3ee589bd0
Ocelot5836 <FinntheRaider@gmail.com>
2020-04-20 23:41:43 -0700

    Updated tests

--- src/test/java/io/github/ocelot/testmod/block/TestBlock.java
+++ src/test/java/io/github/ocelot/testmod/block/TestBlock.java
@@ -1,9 +1,12 @@
 package io.github.ocelot.testmod.block;
 
+import io.github.ocelot.common.BaseBlock;
 import io.github.ocelot.common.VoxelShapeHelper;
 import io.github.ocelot.testmod.tileentity.TestTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -13,9 +16,9 @@ import net.minecraft.world.IBlockReader;
 
 import javax.annotation.Nullable;
 
-public class TestBlock extends Block
+public class TestBlock extends BaseBlock implements IWaterLoggable
 {
-    private static final VoxelShape SHAPE = new VoxelShapeHelper.Builder().append(Block.makeCuboidShape(4, 0, 4, 12, 8, 12), Block.makeCuboidShape(5, 8, 5, 11, 16, 11)).rotate(Direction.DOWN).build();
+    private static final VoxelShape SHAPE = new VoxelShapeHelper.Builder().append(Block.makeCuboidShape(4, 0, 4, 12, 8, 12), Block.makeCuboidShape(5, 8, 5, 11, 16, 11)).rotate(Direction.NORTH).build();
 
     public TestBlock(Block.Properties properties)
     {
@@ -39,5 +42,11 @@ public class TestBlock extends Block
     public TileEntity createTileEntity(BlockState state, IBlockReader world)
     {
         return new TestTileEntity();
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(WATERLOGGED);
     }
 }
