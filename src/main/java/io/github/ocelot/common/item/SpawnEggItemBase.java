a1849802cc7cb07de1120930b88e3569a20d0c47
Ocelot <FinntheRaider@gmail.com>
2020-07-16 16:21:50 -0700

    Added the ability to insert spawn eggs after vanilla eggs

--- src/main/java/io/github/ocelot/common/item/SpawnEggItemBase.java
+++ src/main/java/io/github/ocelot/common/item/SpawnEggItemBase.java
@@ -2,11 +2,16 @@ package io.github.ocelot.common.item;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.RegistryObject;
 
 import javax.annotation.Nullable;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * <p>A spawn egg that allows for deferred entity types.</p>
@@ -17,12 +22,45 @@ import javax.annotation.Nullable;
 @SuppressWarnings("unused")
 public class SpawnEggItemBase<T extends Entity> extends SpawnEggItem
 {
+    private final boolean addToMisc;
     private final RegistryObject<EntityType<T>> type;
 
+    /**
+     * @deprecated Use alternate constructor. TODO remove in 4.0.0
+     */
     public SpawnEggItemBase(RegistryObject<EntityType<T>> type, int primaryColor, int secondaryColor, Properties builder)
+    {
+        this(type, primaryColor, secondaryColor, false, builder);
+    }
+
+    public SpawnEggItemBase(RegistryObject<EntityType<T>> type, int primaryColor, int secondaryColor, boolean addToMisc, Properties builder)
     {
         super(EntityType.OCELOT, primaryColor, secondaryColor, builder);
         this.type = type;
+        this.addToMisc = addToMisc;
+    }
+
+    @Override
+    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items)
+    {
+        if (!this.addToMisc)
+        {
+            super.fillItemGroup(group, items);
+            return;
+        }
+
+        if (this.isInGroup(group) || group == ItemGroup.MISC)
+        {
+            Optional<ItemStack> optional = items.stream().filter(stack -> stack.getItem() instanceof SpawnEggItem && "minecraft".equals(Objects.requireNonNull(stack.getItem().getRegistryName()).getNamespace())).reduce((a, b) -> b);
+            if (optional.isPresent() && items.contains(optional.get()))
+            {
+                items.add(items.indexOf(optional.get()) + 1, new ItemStack(this));
+            }
+            else
+            {
+                items.add(new ItemStack(this));
+            }
+        }
     }
 
     @Override
