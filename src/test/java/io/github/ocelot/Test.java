9f7d1fb60f8b9d134f9e7d57ee6ef205d61538b6
Ocelot <FinntheRaider@gmail.com>
2020-07-17 10:20:08 -0700

    Update Test.java

--- src/test/java/io/github/ocelot/Test.java
+++ src/test/java/io/github/ocelot/Test.java
@@ -1,51 +1,29 @@
 package io.github.ocelot;
 
-import io.github.ocelot.common.OnlineRequest;
-import org.apache.commons.io.IOUtils;
-
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Test
 {
     public static void main(String[] args) throws Exception
     {
-        OnlineRequest.Request request = OnlineRequest.make("http://ipv4.download.thinkbroadband.com/1GB.zip", stream -> {
-            try
+        BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        for (int i = 0; i < 16; ++i)
+        {
+            for (int j = 0; j < 16; ++j)
             {
-                IOUtils.toBufferedInputStream(stream);
+                if (i < 8)
+                {
+                    image.setRGB(j, i, 0xFFB20000);
+                }
+                else
+                {
+                    int k = (int) ((1.0F - (float) j / 15.0F * 0.75F) * 255.0F);
+                    image.setRGB(j, i, k << 24 | 16777215);
+                }
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }, Throwable::printStackTrace);
-        Thread.sleep(4000);
-        request.cancel();
-//            OnlineRequest.Request request = OnlineRequest.make("http://ipv4.download.thinkbroadband.com/50MB.zip", null, Throwable::printStackTrace)
-//            double currentProgress = -1;
-//            while (!request.isCancelled() && !request.getValue().isPresent())
-//            {
-//                if (currentProgress != request.getDownloadPercentage())
-//                {
-//                    System.out.println(currentProgress = request.getDownloadPercentage());
-//                }
-//                if (currentProgress >= 0.1)
-//                {
-//                    System.out.println("Cancelling");
-//                    request.cancel();
-//                }
-//            }
-//            if (request.isCancelled())
-//            {
-//                System.out.println("Download was cancelled");
-//            }
-//            else
-//            {
-//                System.out.println("Download took " + (System.currentTimeMillis() - request.getStartTime()) + "ms");
-//                try (FileOutputStream os = new FileOutputStream("50MB.zip"))
-//                {
-//                    IOUtils.copy(request.getValue().get(), os);
-//                }
-//            }
+        }
+        ImageIO.write(image, "PNG", new File("overlay.png"));
     }
 }
