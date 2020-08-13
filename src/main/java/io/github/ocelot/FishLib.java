6a9100e681923c163026aac30c4f431a49b172e6
Ocelot <FinntheRaider@gmail.com>
2020-08-12 21:35:49 -0700

    Added tooltip coloring framework

---
+++ src/main/java/io/github/ocelot/FishLib.java
@@ -0,0 +1,15 @@
+package io.github.ocelot;
+
+/**
+ * <p>Contains static information about Fish Lib.</p>
+ *
+ * @author Ocelot
+ * @since 3.2.0
+ */
+public class FishLib
+{
+    /**
+     * The domain (modid) used for resource locations.
+     */
+    public static final String DOMAIN = "fishlib";
+}
