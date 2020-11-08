f8ad47aed6b2cc8fcb981e78cacc54247300d4fa
Ocelot <FinntheRaider@gmail.com>
2020-11-07 17:21:36 -0800

    Update TestClientHandler.java

--- src/test/java/io/github/ocelot/sonar/network/TestClientHandler.java
+++ src/test/java/io/github/ocelot/sonar/network/TestClientHandler.java
@@ -1,13 +1,46 @@
 package io.github.ocelot.sonar.network;
 
+import io.github.ocelot.sonar.Sonar;
+import io.github.ocelot.sonar.client.screen.ValueContainerEditorScreenImpl;
+import io.github.ocelot.sonar.common.valuecontainer.IValueContainerClientHandler;
+import io.github.ocelot.sonar.common.valuecontainer.SyncValueContainerMessage;
+import io.github.ocelot.sonar.common.valuecontainer.ValueContainer;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.network.PacketDistributor;
+
+import javax.annotation.Nullable;
 
 @OnlyIn(Dist.CLIENT)
-public class TestClientHandler
+public class TestClientHandler implements IValueContainerClientHandler
 {
     public TestClientHandler()
     {
         System.out.println("CREATING NEW CLIENT HANDLER");
+    }
+
+    @Nullable
+    @Override
+    public Screen createValueContainerScreen(ValueContainer container, BlockPos pos)
+    {
+        ResourceLocation textureLocation = new ResourceLocation(Sonar.DOMAIN, "textures/gui/value_container_editor.png");
+        return new ValueContainerEditorScreenImpl(container, pos, () -> new StringTextComponent("Value Container Editor Test"))
+        {
+            @Override
+            public ResourceLocation getBackgroundTextureLocation()
+            {
+                return textureLocation;
+            }
+
+            @Override
+            protected void sendDataToServer()
+            {
+                TestMessageHandler.PLAY.send(PacketDistributor.SERVER.noArg(), new SyncValueContainerMessage(this.getPos(), this.getEntries()));
+            }
+        };
     }
 }
