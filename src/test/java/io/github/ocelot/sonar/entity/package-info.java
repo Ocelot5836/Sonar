7c4e39c7c988ae82569f7602d00bb9ba334bddf1
Ocelot <FinntheRaider@gmail.com>
2020-11-09 08:48:20 -0800

    Code cleanup

---
+++ src/test/java/io/github/ocelot/sonar/entity/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package io.github.ocelot.sonar.entity;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
