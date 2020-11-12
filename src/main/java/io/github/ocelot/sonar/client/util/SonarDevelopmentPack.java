7773855214f1761836510123897676cd9481244e
Ocelot <FinntheRaider@gmail.com>
2020-11-12 10:08:51 -0800

    Merge branch '1.16' into 1.16.3

---
+++ src/main/java/io/github/ocelot/sonar/client/util/SonarDevelopmentPack.java
@@ -0,0 +1,121 @@
+package io.github.ocelot.sonar.client.util;
+
+import com.google.gson.Gson;
+import io.github.ocelot.sonar.Sonar;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.*;
+import net.minecraft.resources.data.IMetadataSectionSerializer;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.apache.commons.io.IOUtils;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SonarDevelopmentPack extends ResourcePack
+{
+    private static final Gson GSON = new Gson();
+    private String[] resources;
+
+    public SonarDevelopmentPack()
+    {
+        super(new File("Sonar Resources"));
+    }
+
+    public static void init()
+    {
+        if (FMLLoader.isProduction())
+            return;
+        Minecraft.getInstance().getResourcePackList().addPackFinder(new IPackFinder()
+        {
+            @Override
+            public void findPacks(Consumer<ResourcePackInfo> packs, ResourcePackInfo.IFactory packInfoFactory)
+            {
+                ResourcePackInfo t1 = ResourcePackInfo.createResourcePack(Sonar.DOMAIN + "_dev", true, SonarDevelopmentPack::new, packInfoFactory, ResourcePackInfo.Priority.TOP, IPackNameDecorator.BUILTIN);
+                if (t1 != null)
+                {
+                    packs.accept(t1);
+                }
+            }
+        });
+    }
+
+    private String[] getResources() throws IOException
+    {
+        if (this.resources == null)
+        {
+            this.resources = JSONUtils.fromJson(GSON, IOUtils.toString(SonarDevelopmentPack.class.getResourceAsStream("/" + Sonar.DOMAIN + "_resources.json"), StandardCharsets.UTF_8), String[].class);
+            if (this.resources == null)
+                this.resources = new String[0];
+        }
+
+        return this.resources;
+    }
+
+    @Override
+    protected InputStream getInputStream(String resourcePath) throws IOException
+    {
+        return SonarDevelopmentPack.class.getResourceAsStream("/" + resourcePath);
+    }
+
+    @Override
+    public boolean resourceExists(String resourcePath)
+    {
+        try
+        {
+            return Arrays.asList(this.getResources()).contains(resourcePath);
+        }
+        catch (IOException ignored)
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public Set<String> getResourceNamespaces(ResourcePackType type)
+    {
+        return Collections.singleton(Sonar.DOMAIN);
+    }
+
+    @Override
+    public void close()
+    {
+    }
+
+    @Override
+    public Collection<ResourceLocation> getAllResourceLocations(ResourcePackType type, String namespaceIn, String pathIn, int maxDepthIn, Predicate<String> filterIn)
+    {
+        try
+        {
+            return Arrays.stream(this.getResources()).map(resource -> new ResourceLocation(Sonar.DOMAIN, resource.substring(8 + Sonar.DOMAIN.length()))).collect(Collectors.toSet());
+        }
+        catch (IOException e)
+        {
+            return Collections.emptySet();
+        }
+    }
+
+    @Nullable
+    @Override
+    public <T> T getMetadata(IMetadataSectionSerializer<T> deserializer) throws IOException
+    {
+        T object;
+        try (InputStream inputstream = this.getInputStream(Sonar.DOMAIN + "_pack.mcmeta"))
+        {
+            object = getResourceMetadata(deserializer, inputstream);
+        }
+
+        return object;
+    }
+}
